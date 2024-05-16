bdf4e8b47755ac93b2a621dc8ee346e35cce151b
Joris Falter <joris@jorisfalter.com>
2024-05-16 09:29:58 +0700

    x

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,45 @@ function Home() {
   return (
     <div>
       Welcome to My Portfolio
-      <p>Project 1: AI Wrapping in AI-Juniors</p>
-      <p>Project 2: Frontend development in Reggio-Home (Using Next)</p>
-      <p>Project 3: AI Coach</p>
-      <p>Project 4: FlightRadar API usage for wheredoesthea380fly</p>
-      <p>Project 5: FlightAware API usage for FlightsForFlaneurs</p>
-      <p>Project 6: Fun Marketing Tools</p>
+      <p>Project 1: ChatGPT AI Wrapping in AI-Juniors</p>
+      <p>Project 2: Frontend development for Reggio-Home (using Next)</p>
+      <p>Project 3: AI Coach, with a Python backend </p>
+      <p>
+        Project 4: FlightRadar API usage for wheredoesthea380fly, Node backend.
+        Storage in MongoDB.
+      </p>
+      <p>
+        Project 5: FlightAware API usage for FlightsForFlaneurs, Node Backend.
+        Storage in MongoDB.
+      </p>
+      <p>Project 6: Read Vertical Like the Japanese: in Google App Script</p>
+      <p>
+        Project 7: Raspberry Pi Project which turns a light on when a specific
+        bascule bridge in the Netherlands opens. Hardware in micro python +
+        Small scraping part because the Dutch government didn't gave my api
+        access.
+      </p>
+      <p>
+        Project 8: Automatic check if new jobs are posted to a freelance
+        platform. Sends emails with new jobs content using Postmark.{" "}
+      </p>
+      <p>
+        Project 9: Notion tool to reread my goals, principles, marketing plans,
+        project plans, ... with a Python backend.{" "}
+      </p>
+      <p>Project 10: A selection of Fun Marketing Tools</p>
+      <ul>
+        <li>...</li>
+        <li>...</li>
+      </ul>
+      <p>
+        Project 11: The project I got the most attention with up to now:
+        RandomAirport. A NodeJS backend and a React frontend. Uses the Google
+        Maps API. High dependency on Jetphotos.{" "}
+      </p>
+      <p>
+        Project 12: SameDayBlocker: A Google Calendar integration in App Script.
+      </p>
     </div>
   );
 }
